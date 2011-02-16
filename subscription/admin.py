3ad1fa24a99f77f1db7be1fa471030b069c8fc7a
Rodrigo Paiva <rodrigo.paiva@onisolucoes.net>
2011-02-16 14:21:36 -0300

    liberando admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from subscription.models import Subscription
 
-admin.site.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created_at')
+
+admin.site.register(Subscription, SubscriptionAdmin)
 
