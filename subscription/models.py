a23b5f46237c04c2e728def314fae72f5f6dc1d6
Rodrigo Paiva <rodrigo.paiva@onisolucoes.net>
2011-02-16 14:12:39 -0300

    mais um detalhe

--- subscription/models.py
+++ subscription/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11, unique=True)
+    email = models.EmailField('Email', unique=True)
+    phone = models.CharField('Telefone', max_length=20, blank=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
