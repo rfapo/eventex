463a01662722d1a70942766fef3ed195ec12e6c8
Rodrigo Paiva <rodrigo.paiva@onisolucoes.net>
2011-02-16 14:08:42 -0300

    mais uma tentativa

--- subscription/urls.py
+++ subscription/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('subscription.views',
     url(r'^$', 'subscribe', name='subscribe'),
-    route(r'^(\d+)/sucesso/$', GET='new', POST='create', name='success'),
+    url(r'^(\d+)/sucesso/$', 'success', name='success'),
 )
+
 
