f60d0f57ab74087af6161dc34b23fdc9cbb194e1
Rodrigo Paiva <rodrigo.paiva@onisolucoes.net>
2011-03-02 22:21:58 -0300

    ainda no sprint III

--- subscription/admin.py
+++ subscription/admin.py
@@ -35,7 +35,9 @@ class SubscriptionAdmin(admin.ModelAdmin):
         rows = [','.join([s.name, s.email]) for s in subscriptions]
         response = HttpResponse('\r\n'.join(rows))
         response.mimetype = "text/csv"
+
         response['Content‐Disposition'] = 'attachment; filename=inscricoes.csv'
+
         return response
 
     def get_urls(self):
