f87b1ac714fe76d4aaf69ea34889615346ed0a2c
Rodrigo Paiva <rodrigo.paiva@onisolucoes.net>
2011-03-03 09:39:12 -0300

    correcao do admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -35,7 +35,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
         rows = [','.join([s.name, s.email]) for s in subscriptions]
         response = HttpResponse('\r\n'.join(rows))
         response.mimetype = "text/csv"
-        response['Content-Disposition'] = 'attachment; filename=inscricoes.csv'
+        #response['Content-Disposition'] = 'attachment; filename=inscricoes.csv'
         return response
 
     def get_urls(self):
